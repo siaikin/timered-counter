a6d180459278c6a49adf493cbe345a04c2be414f
siaikin <abc1310054026@outlook.com>
2025-03-24 17:08:01 +0800

    feat: 支持通过 cdn 使用 TimeredCounter

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+// Import rollup plugins
+// import { copy } from '@web/rollup-plugin-copy';
+import 'rollup';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import summary from 'rollup-plugin-summary';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+export default {
+  input: './dist/src/index.js',
+  plugins: [
+    // Resolve bare module specifiers to relative paths
+    nodeResolve(),
+    // Minify JS
+    // @ts-ignore
+    terser({
+      ecma: 2021,
+      module: true,
+      warnings: true,
+    }),
+    // Print bundle summary
+    // @ts-ignore
+    summary({
+      showBrotliSize: true,
+      showGzippedSize: true,
+      showMinifiedSize: true,
+    }),
+    // Optional: copy any static assets to build directory
+    // copy({
+    //   patterns: ['images/**/*'],
+    // }),
+    visualizer({ gzipSize: true, brotliSize: true }),
+  ],
+  output: [
+    {
+      file: './dist/timered-counter.esm-browser.js',
+      format: 'es',
+      name: 'TimeredCounter',
+      sourcemap: true,
+    },
+    {
+      file: './dist/timered-counter.global.js',
+      format: 'iife',
+      name: 'TimeredCounter',
+      sourcemap: true,
+    },
+  ],
+  preserveEntrySignatures: 'strict',
+};
